f06958e06534379475a45ba582885919133e7af7
ParthSoni-CS <parthsoni08072000@gmail.com>
2024-12-09 21:21:42 -0600

    updates in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 HYPEN_E_DOT='-e .'
-'''def get_requiremet(file_path:str)->List[str]:
+def get_requiremet(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
@@ -12,7 +12,7 @@ HYPEN_E_DOT='-e .'
     return requirements
    
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     '''
+    long_description = f.read()     
    
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
