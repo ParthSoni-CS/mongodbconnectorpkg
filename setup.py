1b0302b331f39f6a8195324a1134fc610c77ecee
ParthSoni-CS <parthsoni08072000@gmail.com>
2024-12-10 04:46:41 -0600

    updates

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requiremet(file_path:str)->List[str]:
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "mongodbconnecterpkg"
 AUTHOR_USER_NAME = "ParthSoni-CS"
